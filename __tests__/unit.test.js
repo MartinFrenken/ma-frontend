5e7200a92be8701a6fb4492565d8cf300249f986
Martin <martin.frenken@gmail.com>
2020-06-20 22:24:19 +0200

    Lets go

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,7 +1,31 @@
 import Axios from "axios";
 import {expect, it} from "@jest/globals";
+import ApiGateway from "../src/logic/ApiGateway";
+class LocalStorageMock {
+    constructor() {
+        this.store = {};
+    }
 
-it('Token is not set serverside', () => {
+    clear() {
+        this.store = {};
+    }
+
+    getItem(key) {
+        return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+        this.store[key] = value.toString();
+    }
+
+    removeItem(key) {
+        delete this.store[key];
+    }
+};
+
+global.localStorage = new LocalStorageMock;
+it('Token is not set on gateway creation', () => {
+    let apiGateway = new ApiGateway();
     expect.assertions(1);
     expect(localStorage.getItem("accessToken")).toBeNull();
 });
