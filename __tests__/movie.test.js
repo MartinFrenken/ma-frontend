2157d319df5b8061a2ab72b55763b6210674f544
Martin <martin.frenken@gmail.com>
2020-06-22 13:09:42 +0200

    Hmm

--- __tests__/movie.test.js
+++ __tests__/movie.test.js
@@ -1,32 +1,12 @@
 import {expect, it} from "@jest/globals";
 import Movie from "../src/model/Movie";
 import ApiGateway from "../src/logic/ApiGateway";
-
+import LocalStorageMock from "../src/logic/LocalStorageMock";
 it('Base url is set properly', () => {
-    class LocalStorageMock {
-        constructor() {
-            this.store = {};
-        }
 
-        clear() {
-            this.store = {};
-        }
-
-        getItem(key) {
-            return this.store[key] || null;
-        }
-
-        setItem(key, value) {
-            this.store[key] = value.toString();
-        }
-
-        removeItem(key) {
-            delete this.store[key];
-        }
-    };
 
     global.localStorage = new LocalStorageMock;
-    let apigw= new ApiGateway();
+  //  let apigw= new ApiGateway();
     console.log(apigw.baseUrl);
     expect(apigw.baseUrl).toEqual("http://localhost/gateway");
 });
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,31 +1,10 @@
 import Axios from "axios";
 import {expect, it} from "@jest/globals";
 import ApiGateway from "../src/logic/ApiGateway";
-class LocalStorageMock {
-    constructor() {
-        this.store = {};
-    }
-
-    clear() {
-        this.store = {};
-    }
-
-    getItem(key) {
-        return this.store[key] || null;
-    }
-
-    setItem(key, value) {
-        this.store[key] = value.toString();
-    }
-
-    removeItem(key) {
-        delete this.store[key];
-    }
-};
+import LocalStorageMock from "../src/logic/LocalStorageMock";
 
 global.localStorage = new LocalStorageMock;
 it('Token is not set on gateway creation', () => {
-    let apiGateway = new ApiGateway();
     expect.assertions(1);
     expect(localStorage.getItem("accessToken")).toBeNull();
 });
